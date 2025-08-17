8bad166fc0e9f3c4ae292c45cbd72604573a6c92
ANUBHAV DUBEY <royalajit83@gmail.com>
2025-08-18 00:32:56 +0530

    change in majority elemnt

--- src/Test_August/MajorityElement.java
+++ src/Test_August/MajorityElement.java
@@ -2,7 +2,7 @@ package Test_August;
 
 public class MajorityElement {
     public static int majorityElement(int[] nums) {
-        int count = 0, candidate = 0;
+        int count = 0 , candidate = 0;
 
         for (int num : nums) {
             if (count == 0) {
@@ -10,6 +10,8 @@ public class MajorityElement {
             }
             count += (num == candidate) ? 1 : -1;
         }
+
+
         return candidate;
     }
 

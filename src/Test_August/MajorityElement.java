5411608dbaff29ec8ca946486f295b8ae9c717ad
ANUBHAV DUBEY <royalajit83@gmail.com>
2025-08-18 00:21:01 +0530

    add new file name majority element

---
+++ src/Test_August/MajorityElement.java
@@ -0,0 +1,20 @@
+package Test_August;
+
+public class MajorityElement {
+    public static int majorityElement(int[] nums) {
+        int count = 0, candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,2,1,1,1,2,2};
+        System.out.println(majorityElement(nums)); // Output: 2
+    }
+}
